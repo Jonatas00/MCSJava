4ba4b64e90c84ce6538c0fd6cfebd8c1528ac86a
Jonatas <djonatas31@gmail.com>
2024-08-15 12:29:10 -0300

    remove import

--- introducaospringboot/src/main/java/com/teste/introducaospringboot/controllers/ProdutoController.java
+++ introducaospringboot/src/main/java/com/teste/introducaospringboot/controllers/ProdutoController.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.teste.introducaospringboot.models.Produto;
