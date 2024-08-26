803a321d13da1d4c314ff910863b1e6d04aef868
Jonatas <djonatas31@gmail.com>
2024-08-26 10:43:55 -0300

    fix Docker

--- introducaospringboot/src/test/java/com/teste/introducaospringboot/IntroducaospringbootApplicationTests.java
+++ introducaospringboot/src/test/java/com/teste/introducaospringboot/IntroducaospringbootApplicationTests.java
@@ -1,13 +1,8 @@
 package com.teste.introducaospringboot;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class IntroducaospringbootApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
